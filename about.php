04430eb5cdae8915b0a9f0d21396f05a1521db8f
SoraShinn <96003862+SoraShinn@users.noreply.github.com>
2021-12-12 16:21:44 +0500

    new site

---
+++ about.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>От ГеймДиза </title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux and BSD.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, bsd, open source, nekotekina, kd11, about">
+<meta name="author" content="RPCS3">
+<meta name="copyright" content="RPCS3">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Немного историй</h1>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					Мы попросили некоторых геймдизайнеров рассказать о их пути в гейм дизайн
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<h2>О чем эта страница? </h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							 На этой странице вы можете услышать истории успешных геймдизайнеров, про их путь в гейм дизайне и их жизнь в принципе.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="user-con-grouped">
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: url('/img/users/avatars/nekotekina.png') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/japan.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							Хидео Кодзима </span>
+							<span class="user-con-role user-role-lead">
+							Успешный Геймдизайнер </span>
+						</div>
+						<div class='user-con-divider role-lead darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Когда я увидел презентацию PlayStation, то сразу понял, что теперь смогу сделать игру, которую хочу. С её многообразием цветов и полигонов. Я мог бы сделать игру, которая не использует CGI-видео и работает в реальном времени. В которой действие как в фильме плавно переходит от одной сцены к другой, а не от красивого CGI к низкополигональному геймплею.
+
+
+До PlayStation игры были похожи на театр бумажных кукол. Чтобы нарисовать одну двухмерную картинку, приходилось тратить кучу усилий. На результат нельзя было посмотреть с другого ракурса. Но с выходом первой PlayStation плоские картинки превратились в полигоны. Появилась возможность работать с камерой. Игрок теперь тоже мог её двигать — благодаря разработчикам, конечно, но всё-таки. Всё было как во сне.
+
+
+Слово Solid в Metal Gear Solid имеет три значения. Прежде всего, Solid Snake — это имя персонажа. Затем слово Solid («твёрдый», «сплошной», «трёхмерный») — трёхмерная игра. Третье значение связано со Square («Квадрат»), компанией, которая известна серией Final Fantasy. Квадрат — вещь двухмерная, плоская. И я думаю, президент Konami хотел, чтобы эта игра превзошла Square. Абстрактно мы видели MGS кубом, солидным 3D-объектом. Это был прямой ответ нашему конкуренту. </span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Desktop</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/cpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>AMD Ryzen 5 2600</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/gpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>GTX 1060</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/ram.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>16GB DDR4 (3000MHz)</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/os.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span> Windows 10</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="user-con-content darkmode-txt">
+					<div class='user-img-avatar' style="background: #646E77 url('/img/users/avatars/kd11.png') no-repeat center; background-size: cover;">
+						<div class='user-img-flag' style="background: url('/img/users/flags/south-korea.png') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class="user-con-wrapper">
+						<div class='user-tx1-content darkmode-txt'>
+							<span class="user-tx2-member ">
+							Кори Барлог </span>
+							<span class="user-con-role user-role-graphics">
+							Еще один успешный Гейм Дизайнер</span>
+						</div>
+						<div class='user-con-divider role-graphics darkmode-divider'>
+						</div>
+						<div class='user-tx3-content darkmode-txt'>
+							<span>
+							Это было... очень необычно... и одновременно трогательно. Если честно, я не ожидал, что настолько расчувствуюсь от мнений журналистов. Это застало меня врасплох.
+ 
+Я снял десятиминутное видео с собственной реакцией, восемь из этих минут я плакал, не в силах произнести ни одного внятного слова, настолько я был горд нашей командой, которая успешно пережила пять сложнейших лет разработки игры. Я долго думал, выкладывать ли этот ролик, в итоге решил сделать это ради моего сына Хэло. Он не хочет видеть нас, когда ему грустно, убегая в другую комнату и крича, если мы попробуем войти.
+ 
+Для нас важно показать ему, что грустить — нормально. Плакать — нормально. Этого не надо прятать. Я своим примером покажу ему, что даже папа может расплакаться перед всем миром (или 50 людьми, которые в итоге посмотрят это!).
+ 
+Это для тебя, Хэло. Папа любит тебя. </span>
+						</div>
+						<div class="user-con-specsheet">
+							<div class="user-con-specbox">
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/desktop.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Desktop</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/cpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>AMD R5 3600Х</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/gpu.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>NVIDIA GTX 1660Super</span>
+									</div>
+								</div>
+								
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/ram.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>16GB DDR4 (3200MHz)</span>
+									</div>
+								</div>
+								<div class="specbox-con-part darkmode-invert">
+									<div class="specbox-ico-part" style="background: url('/img/icons/list/os.png') no-repeat center;">
+									</div>
+									<div class="specbox-tx1-part">
+										 <span>Windows 10</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+					
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Геймдизайнер</title>
+<meta charset="utf-8">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/dualshock3.png" width="1" height="1" alt="Preload"/>
+<img src="D:/OpenServer2/domains/Praktika/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.png" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/ui-main-video.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'i9r9NLliDI4',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'CT_F10KCU-k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'ITxMPLPSaAc',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+		<div class="wavebar-con-container-master">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>ГЕЙМДИЗАЙНЕР<br>
+							 Кто он и чем он занимается.</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 Подробное пособие, кто такой Гейм дизайнер<br>
+								 А так же задания для выполнения.
+							</p>
+							
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/papichold.jpg) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="search-con-outer">
+				
+			<div class='adsense-tx1-description darkmode-txt'>
+			<span>Учебный Раздел</span>
+				</div>		
+				
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-video darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-btn-left darkmode-arrow-left left" href="#myCarousel" data-slide="prev">
+			</div>
+			<div class="content-btn-right darkmode-arrow-right right" href="#myCarousel" data-slide="next">
+			</div>
+			<div class="content-con-inside">
+				<div class="video-tx1-heading darkmode-txt">
+					<h2>Видео по теме</h2>
+				</div>
+				<?php include 'lib/module/ui-main-carousel.php';?>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Задания для выполнения</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 Перейде по этой ссылке, вы перейдете на тест, в котором вам нужно написать краткий и полный ответ на вопросы.
+									 Желаем успехов и удачи, в прохождении этого теста.<br> Мы используем Google Forms, т.к там можно удобно сделать полный анализ ответов.
+								</p>
+							</div>
+							<a href="https://forms.gle/HnMi5gVUp3WGpZv29" target="_blank">
+							<div class="github-btn-button">
+								<div class="github-ico-button">
+								</div>
+								<div class="github-tx1-button">
+									<span>Перейти на тест</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>Обратная связь</span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Помоги стать лучше</h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									 Мы используем Google Формы для того, чтобы вы могли оставлять свои отзывы по нашему сайту, а так же могли оставлять свои пожелания.
+									 Большое спасибо, что пользуетесь нами. 
+								</p>
+							</div>
+							<a href="https://forms.gle/rnCYRSMUwX2RFVXM6" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreon-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>Оставить отзыв</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-discord darkmode-discord">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>Для Комьюнити</h2>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 Мы используем Discord для связи с нашим комьюнити. Если вам что-то не понятно, то можете перейти в дискорд и задать интересующие вас вопросы. На сервере хорошая администрация, которая всегда вам поможет.
+								</p>
+							</div>
+							<a href="https://discord.gg/YpC8unnU" target="_blank">
+							<div class="discord-btn-button">
+								<div class="discord-ico-button">
+								</div>
+								<div class="discord-tx1-button">
+									<span>Перейти в Discord</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
